77c2eea08a200d2b28323666e4bedf81a0ad97c9
Zach Greenvoss <z_greenvoss@hotmail.com>
2012-05-16 14:47:35 -0700

    Add script to combine metric and MPI data into new file.  Updated readme.

---
+++ CombineRunDataMetricData.py
@@ -0,0 +1,89 @@
+import os
+import sys
+import datetime
+
+timeDataCollection = []
+mpiResults = []
+
+def main():
+    
+    if (len(sys.argv) < 2):
+        print("Usage: python CombineRunDataMetricData.py <TimeData.csv> <MPIResults.csv> <OutputFile.csv>")
+        quit()
+    else:
+        timeDataFile = sys.argv[1]
+    
+    if (len(sys.argv) < 3):
+        print("Usage: python CombineRunDataMetricData.py <TimeData.csv> <MPIResults.csv> <OutputFile.csv>")
+        quit()
+    else:
+        mpiResultsFile = sys.argv[2]
+    
+    if (len(sys.argv) < 4):
+        print("Usage: python CombineRunDataMetricData.py <TimeData.csv> <MPIResults.csv> <OutputFile.csv>")
+        quit()
+    else:
+        outputFile = sys.argv[3]
+        
+    print "Opening TimeData file: " + timeDataFile
+    
+    fileData = open(timeDataFile,"r")
+    while fileData:
+            line = fileData.readline()
+            if line == "":
+                break;
+            lineData = line.split(",")
+            timeDataCollection.append(lineData)
+    
+    print "Opening MPI Results file: " + mpiResultsFile
+    
+    fileData = open(mpiResultsFile,"r")
+    while fileData:
+            line = fileData.readline()
+            if line == "":
+                break;
+            lineData = line.split(",")
+            mpiResults.append(lineData)
+    
+    newCollection = []
+    
+    print "Adding MPI data into time data..."
+    index = 0
+    for timeDataRow in timeDataCollection:
+        mpiData = mpiResults[index]
+        try:
+            if index > 0 and datetime.datetime.strptime(mpiData[0],"%Y-%m-%d %H:%M:%S.%f") != datetime.datetime.strptime( timeDataRow[0],"%Y-%m-%d %H:%M:%S.%f"):
+                print "timestamps not equal..." + str(mpiData[0]) + " - " + str(timeDataRow[0])
+                quit()
+        except ValueError:
+            #when the MPI results are written out the datetime formatting will drop the milliseconds if they equal 000
+            #   This catch will re-compare with the date format only down to seconds
+            if index > 0 and datetime.datetime.strptime(mpiData[0],"%Y-%m-%d %H:%M:%S") != datetime.datetime.strptime( timeDataRow[0],"%Y-%m-%d %H:%M:%S.%f"):
+                print "timestamps not equal..." + str(mpiData[0]) + " - " + str(timeDataRow[0])
+                quit()
+
+        index += 1
+        #for mpiDataItem in mpiData:
+        for x in range(1,len(mpiData)): #skip time attribute
+            mpiDataItem = mpiData[x]
+            if mpiDataItem.strip() != "":
+                if mpiDataItem[-1] == "\n":
+                    timeDataRow.append(mpiDataItem[0:-1])
+                else:
+                    timeDataRow.append(mpiDataItem)
+        
+        newCollection.append(timeDataRow)
+        
+    print "Writing file..."
+    fileData = open(outputFile,"w")
+    for data in newCollection:
+        for rowData in data:
+            if str(rowData).strip() == "":
+                fileData.write(str(rowData.strip()))
+            else:
+                fileData.write(str(rowData.strip()) + ",")
+        fileData.write("\n")
+    fileData.close()
+    
+    
+main()
--- README.md
+++ README.md
@@ -4,20 +4,23 @@ DataCleansingTool
 This is a set of Python scripts used to cleanse and analyze the output data from the PNNL's Energy Smart Data Center Facility.
 Each script is used as part of a pipeline process to get the data into an integrated and useful form for holistic analysis.
 
-+ParseMetricData.py
+ParseMetricData.py
 -This script is the first step in the pipeline.  It will parse through a directory containing the metric data and gather all metric runs into a single large csv file.  This file will have the timestamp as the first column, and the superset of all captured metrics as the columns.
 -Note that this produces a fairly sparse matrix of data - as the metrics are captured at different timestamps so much of the timestamps have blank cells for the metric data
 
-+PopulateSparseData.py
+PopulateSparseData.py
 -This script consumes the csv file produced by the previous script and fills in the missing components of the sparse matrix.
 -It does this by analyzing each metric "column" and finding the appropriate metric reading to fill in for each timestamp.
 -The output of this process is a completely filled in matrix of timestamp/metric readings
 
-+GenerateRunData.py
+GenerateRunData.py
 -This script reads the MPI measurement data from a specific run of the NWChem chemical package.
 -It uses the timeslices defined in ParseMetricData.py to divide the measurement data into slices and then counts each of the MPI events occurring within each time slice.
 -The final output of this script is a complete picture of the MPI calls for the NWChem application (across all nodes) for each timestamp period.  This can be directly compared with the output of PopulateSparseData.py to analyze the performance and power metric data.
 
+CombineRunDataMetricData.py
+-This combines the previous two output files into a new csv file.
+-This new file will contain all the metric data for each available time slice, and will now contain all the MPI counts for the given time slice as well.
 
 --------------------------------
 
@@ -34,3 +37,6 @@ Assuming the DataCleansingTool directory is installed at the same level as the e
     DataCleansingTool: python GenerateRunData.py output/TimeData1.csv ../LD_A1_56p_2ppn_28n_IO-BASIC_even_RAWDATA/t1/output/sysmpi-parsed/  output/MPIResultsTest1.csv
     DataCleansingTool: python GenerateRunData.py output/TimeData2.csv ../LD_A1_56p_2ppn_28n_IO-BASIC_even_RAWDATA/t2/output/sysmpi-parsed/ output/MPIResultsTest2.csv
     DataCleansingTool: python GenerateRunData.py output/TimeData3.csv ../LD_A1_56p_2ppn_28n_IO-BASIC_even_RAWDATA/t3/output/sysmpi-parsed/ output/MPIResultsTest3.csv
+    DataCleansingTool: python CombineRunDataMetricData.py output/TimeData1.csvConverted.csv output/MPIResultsTest1.csv output/combinedData1.csv
+    DataCleansingTool: python CombineRunDataMetricData.py output/TimeData2.csvConverted.csv output/MPIResultsTest2.csv output/combinedData2.csv
+    DataCleansingTool: python CombineRunDataMetricData.py output/TimeData3.csvConverted.csv output/MPIResultsTest3.csv output/combinedData3.csv
