c5156a7ad3b758758a45a29a0fd7682f06828136
Zach Greenvoss <z_greenvoss@hotmail.com>
2012-05-21 00:47:36 -0700

    Added ability to specify time slices in milliseconds for a more normalized output.

--- ParseMetricData.py
+++ ParseMetricData.py
@@ -8,9 +8,13 @@ timeCollection3 = {}
 metricsRun1 = []
 metricsRun2 = []
 metricsRun3 = []
+normalizedTimeCollection1 = {}
+normalizedTimeCollection2 = {}
+normalizedTimeCollection3 = {}
 
 def getMetricNames(file,testRunDataNumber):
     #get metric name
+    #print "GetMetricNames - " + str(file)
     fileNoExt = os.path.splitext(file)[0]
     x = fileNoExt.find("t0" + str(testRunDataNumber))
     if x < 0:
@@ -36,7 +40,7 @@ def getMetricNames(file,testRunDataNumber):
     return
 
 def gatherMetrics(folder):
-    print folder
+    #print "Gather Metrics - " + folder
     if os.path.isdir(folder):
         ext = os.listdir(folder)
         for e in ext:
@@ -46,34 +50,145 @@ def gatherMetrics(folder):
                 getMetricNames(os.path.join(folder,e),2)
             elif e.find("t03") >= 0:
                 getMetricNames(os.path.join(folder,e),3)
+            else:
+                print "Invalid file - " + e
             
+
+
+
+def updateTimeSlices(timeCollection,destTimeCollection,timeDelta):
+    currentBottomTime = datetime.datetime.strptime(sorted(timeCollection.iterkeys())[0],"%Y-%m-%d %H:%M:%S.%f") - datetime.timedelta(0,0,0,timeDelta)
+    currentTopTime = currentBottomTime + datetime.timedelta(0,0,0,timeDelta)
+        
+    while True:
+        tempCollection = ["" for x in range(len(metricsRun1))]
+        count = 0
+        #print "Bottom: " + str(currentBottomTime)
+        #print "Top: " + str(currentTopTime)
+        for key in sorted(timeCollection.iterkeys()):
+            currentTime = datetime.datetime.strptime(str(key),"%Y-%m-%d %H:%M:%S.%f")
+            if currentTime > currentBottomTime and currentTime <= currentTopTime:
+                #metric row within time slice
+                #print "Valid Metric Found. CurrentTime: " + str(currentTime)
+                #print "     Bottom: " + str(currentBottomTime)
+                #print "     Top: " + str(currentTopTime)
+                #print "     " + str(timeCollection1[key])
+                
+                count += 1
+                for x in range(0,len(timeCollection[key])):
+                    
+                    if tempCollection[x] == "":
+                        if timeCollection[key][x] == "":
+                            currentVal = 0.0
+                        else:
+                            currentVal = float(timeCollection[key][x])
+                    else:
+                        currentVal = float(tempCollection[x])
+                        
+                    if timeCollection[key][x] == "":
+                        newVal = 0.0
+                    else:
+                        newVal = float(timeCollection[key][x])
+                    #if x == 0:
+                    #    print "currentVal: " + str(currentVal) + " newVal: " + str(newVal)
+                    #calculate running average...
+                    if float(newVal) != 0.0:
+                        tempCollection[x] = (((count - 1) * currentVal) + newVal)/float(count)
+                    #print "currentVal: " + str(currentVal) + " newVal: " + str(newVal) + " - " + str(tempCollection[x])
+            elif currentTime > currentBottomTime and currentTime > currentTopTime:
+                    #outside time slice
+                    break     
+        
+        #print "Done with time slice - adding " + str(len(tempCollection)) + " metric into slice. count: " + str(count)
+        destTimeCollection[currentBottomTime] = tempCollection
+        
+        #advance time window
+        currentBottomTime = currentTopTime + datetime.timedelta(0,0,0,1)
+        currentTopTime = currentTopTime + datetime.timedelta(0,0,0,timeDelta)
+    
+        #check for exit
+        if datetime.datetime.strptime(sorted(timeCollection.iterkeys())[-1],"%Y-%m-%d %H:%M:%S.%f") < currentBottomTime:
+            break;
+    return
+
+#datetime.datetime.strptime(splitData[1][1:] + " " + splitData[2][:-1],"%Y-%m-%d %H:%M:%S.%f")])
+
+def addToTimeSlice(file,testRunDataNumber,timeDelta):
+    #print file
+    timeData = []
+    currentBottomTime = datetime.datetime(1900,1,1)
+    
+    #open file
+    index = 0
+    fileData = open(file,"r")
+    while fileData:
+        line = fileData.readline()
+        if len(line) == 0:
+            break;
+        #time data
+        lineData = line.split(",")
+        dateField = lineData[0]
+        valueField = lineData[1].strip()
+        if testRunDataNumber == 1:
+            if timeCollection1.has_key(dateField):
+                tempCollection = timeCollection1.get(dateField)
+            else:
+                tempCollection = ["" for x in range(len(metricsRun1))]
             
-def gatherMetricData(folder):
-    print folder
+            index = metricsRun1.index(metricName)
+            tempCollection[index] = valueField
+            timeCollection1[dateField] = tempCollection
+        elif testRunDataNumber == 2:
+            if timeCollection2.has_key(dateField):
+                tempCollection = timeCollection2.get(dateField)
+            else:
+                tempCollection = ["" for x in range(len(metricsRun2))]
+            
+            index = metricsRun2.index(metricName)
+            tempCollection[index] = valueField
+            timeCollection2[dateField] = tempCollection
+        elif testRunDataNumber == 3:
+            if timeCollection3.has_key(dateField):
+                tempCollection = timeCollection3.get(dateField)
+            else:
+                tempCollection = ["" for x in range(len(metricsRun3))]
+            
+            index = metricsRun3.index(metricName)
+            tempCollection[index] = valueField
+            timeCollection3[dateField] = tempCollection
+         
+        index += 1
+    fileData.close()
+    
+
+def gatherMetricData(folder,timeDelta):
+    #print folder
     if os.path.isdir(folder):
         ext = os.listdir(folder)
         for e in ext:
             if e.find("t01") >= 0:
-                addToTestSet(os.path.join(folder,e),1)
+                addToTestSet(os.path.join(folder,e),1,timeDelta)
             elif e.find("t02") >= 0:
-                addToTestSet(os.path.join(folder,e),2)
+                addToTestSet(os.path.join(folder,e),2,timeDelta)
             elif e.find("t03") >= 0:
-                addToTestSet(os.path.join(folder,e),3)
+                addToTestSet(os.path.join(folder,e),3,timeDelta)
             
     return
 
-def addToTestSet(file,testRunDataNumber):
-    print file
+def addToTestSet(file,testRunDataNumber,timeDelta):
+    #print file
     fileNoExt = os.path.splitext(file)[0]
     metricData = []
     #get metric name
     x = fileNoExt.find("t0" + str(testRunDataNumber))
     metricName = fileNoExt[(x+4):]
-    print metricName
+    #print metricName
     #open file
     index = 0
+    #print "Opening " + str(file)
     fileData = open(file,"r")
     while fileData:
+        
         line = fileData.readline()
         if len(line) == 0:
             break;
@@ -127,30 +242,31 @@ def main():
     else:
         print("Output directory set to: " + sys.argv[2])
         outputDir = sys.argv[2]
+        
+    if (len(sys.argv) < 4):
+        print("No time argument specified - use time values as default.")
+        timeDelta = -1
+    else:
+        print("Time argument specified - using time slices of " + sys.argv[3] + " milliseconds.")
+        timeDelta = int(sys.argv[3])
 
     print "Gathering data..."
     
     files = os.listdir(sys.argv[1])
         
-    #Loop through top level directories
+    #Loop through directories - first getting metric names
+    #   then getting metric data
     for file in files:
         gatherMetrics(os.path.join(sys.argv[1],file))
-        
-        #if os.path.isdir(file) and (os.path.split(file)[1].find("POWER-COOLING") >=0):
-        #    ext = os.listdir(file)
-        #    for e in ext:
-        #        gatherMetrics(os.path.join(file,e))
     
     for file in files:
-        gatherMetricData(os.path.join(sys.argv[1],file))
+        gatherMetricData(os.path.join(sys.argv[1],file),timeDelta)
     
+    if timeDelta != -1:
+        updateTimeSlices(timeCollection1,normalizedTimeCollection1,timeDelta)
+        updateTimeSlices(timeCollection2,normalizedTimeCollection2,timeDelta)
+        updateTimeSlices(timeCollection3,normalizedTimeCollection3,timeDelta)
     
-    #for file in files:
-    #    if os.path.isdir(file) and (os.path.split(file)[1].find("POWER-COOLING") >=0):
-    #        ext = os.listdir(file)
-    #        for e in ext:
-    #            gatherMetricData(os.path.join(file,e))
-                
     print "done generating collections..."
     print "writing test data..."
     
@@ -163,6 +279,19 @@ def main():
     for key in sorted(timeCollection1.iterkeys()):
         fileData.write(str(key) + ", ")
         for metricData in timeCollection1[key]:
+            fileData.write(str(metricData) + ", ")
+        fileData.write("\n")
+    fileData.close()
+    
+    fileData = open(os.path.join(outputDir,"NormalizedTimeData1.csv"),"w")
+    fileData.write("Time,")
+    for metric in metricsRun1:
+        fileData.write(metric + ", ")
+    fileData.write("\n")
+    
+    for key in sorted(normalizedTimeCollection1.iterkeys()):
+        fileData.write(str(key) + ", ")
+        for metricData in normalizedTimeCollection1[key]:
             fileData.write(str(metricData) + ", ")
         fileData.write("\n")
     fileData.close()
@@ -180,6 +309,20 @@ def main():
         fileData.write("\n")
     fileData.close()
     
+    
+    fileData = open(os.path.join(outputDir,"NormalizedTimeData2.csv"),"w")
+    fileData.write("Time,")
+    for metric in metricsRun2:
+        fileData.write(metric + ", ")
+    fileData.write("\n")
+    
+    for key in sorted(normalizedTimeCollection2.iterkeys()):
+        fileData.write(str(key) + ", ")
+        for metricData in normalizedTimeCollection2[key]:
+            fileData.write(str(metricData) + ", ")
+        fileData.write("\n")
+    fileData.close()
+    
     fileData = open(os.path.join(outputDir,"TimeData3.csv"),"w")
     fileData.write("Time,")
     for metric in metricsRun3:
@@ -193,5 +336,17 @@ def main():
         fileData.write("\n")
     fileData.close()
     
+    fileData = open(os.path.join(outputDir,"NormalizedTimeData3.csv"),"w")
+    fileData.write("Time,")
+    for metric in metricsRun3:
+        fileData.write(metric + ", ")
+    fileData.write("\n")
+    
+    for key in sorted(normalizedTimeCollection3.iterkeys()):
+        fileData.write(str(key) + ", ")
+        for metricData in normalizedTimeCollection3[key]:
+            fileData.write(str(metricData) + ", ")
+        fileData.write("\n")
+    fileData.close()
     
 main()
